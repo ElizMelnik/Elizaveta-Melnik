bc30e907b5dfb6cf17808cc1d6bfdd54b2bbe570
ElizMelnik <97melliza@mail.ru>
2019-08-13 01:00:19 +0300

    replace

--- src/number.java
+++ src/number.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 
-class number{
-    public static void main(String args[]){
-        System.out.print("Введите число: ");
+class number {
+    public static void main(String args[]) {
+        System.out.print("Введите число: \n");
         Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-
-        if (x > 7) {
-            System.out.printf("Привет");
+        int arr[] = new int[10];
+        for (int i = 0; i < 100; i++) {
+            arr[i] = scan.nextInt();
+            if (arr[i] <= 7) {
+                System.out.printf("Меньше 7\n");
+                System.out.printf("Введите число:\n");
+            } else if (arr[i] > 7) {
+                System.out.printf("Привет");
+                break;
+            }
         }
+
     }
 }
